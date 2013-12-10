5f9b121ee2bfd188f659a2d14da00ec2533d4923
Jean-Christophe Kautzmann <jkautzma@adobe.com>
2013-12-10 16:19:11 +0100

    CQ-10370 - Write code samples showing how to extend the Hybris connector

---
+++ myproject/bundle/src/main/java/com/mycompany/myproject/HelloService.java
@@ -0,0 +1,13 @@
+package com.mycompany.myproject;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
---
+++ myproject/bundle/src/main/java/com/mycompany/myproject/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.mycompany.myproject;
+
+import aQute.bnd.annotation.Version;
