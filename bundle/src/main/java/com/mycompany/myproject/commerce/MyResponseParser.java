4e31b1184c287bf8de92dd06248465c522f33a87
Jean-Christophe Kautzmann <jkautzma@adobe.com>
2014-01-22 14:14:46 +0100

    CQ-12518 - hybris: add code samples showing how to extend the importer with product references

--- bundle/src/main/java/com/mycompany/myproject/commerce/MyResponseParser.java
+++ bundle/src/main/java/com/mycompany/myproject/commerce/MyResponseParser.java
@@ -1,10 +1,16 @@
 package com.mycompany.myproject.commerce;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 
 import com.adobe.cq.commerce.hybris.importer.DefaultResponseParser;
+import com.adobe.cq.commerce.hybris.importer.XMLEventReaderUtil;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
@@ -23,6 +29,34 @@ public class MyResponseParser extends DefaultResponseParser {
     @Override
     public Map<String, Object> parseProductData(XMLEventReader xml, boolean variant) throws Exception {
         return super.parseProductData(xml, variant);
-   }
+    }
+
+    @Override
+    protected void parseUnknownProductAttribute(XMLEventReader xml, Map<String, Object> values, String tagName) throws XMLStreamException {
+        if ("productReferences".equals(tagName)) {
+            final List<String> references = new ArrayList<String>();
+            while (!XMLEventReaderUtil.isNextEndElement(xml, "productReferences")) {
+                final XMLEvent referenceEvent = XMLEventReaderUtil.nextTag(xml);
+                if (!referenceEvent.isStartElement()) continue;
+                final StartElement referenceTag = referenceEvent.asStartElement();
+                final String referenceTagName = referenceTag.getName().getLocalPart();
+                if ("target".equals(referenceTagName)) {
+                    while (!XMLEventReaderUtil.isNextEndElement(xml, "target")) {
+                        final XMLEvent targetEvent = XMLEventReaderUtil.nextTag(xml);
+                        if (!targetEvent.isStartElement()) continue;
+                        final StartElement targetTag = targetEvent.asStartElement();
+                        final String targetTagName = targetTag.getName().getLocalPart();
+                        if ("url".equals(targetTagName)) {
+                            references.add(xml.getElementText());
+                        }
+                    }
+                }
+            }
+            values.put("references", references);
+            XMLEventReaderUtil.skipElement(xml, "productReferences");
+        } else {
+            XMLEventReaderUtil.skipElement(xml, tagName);
+        }
+    }
 
 }
